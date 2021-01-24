4919ba4bc921e9970e9c33f40777b1067aebc34c
Angelica Miranda <am@MacBook-Air-de-Angelica.local>
2021-01-23 21:49:06 -0300

    binomial_distribution_I

---
+++ 10_days_of_statistics_hacker_rank/day04_binomial_distribution_I.R
@@ -0,0 +1,19 @@
+#Hacker Rank
+#10 Days of Statistics
+#Day01
+#Binomial distribution I
+
+data <- scan("/dev/stdin")
+boy <- data[1]
+girl <- data[2]
+p <- boy/(boy+girl)
+q <- girl/(boy+girl)
+n = 6
+x = 3
+ans = 0
+while (x <= 6){
+    n_x = factorial(n)/(factorial(x)*factorial(n-x))
+    ans = ans + (n_x * (p ** x) * (q ** (n - x)))
+    x = x + 1
+}
+cat(format(round(ans, 3), nsmall = 1))
