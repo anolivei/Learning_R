088f7c690494b6dd613d3b622f76ca75fd959a98
Angelica Miranda <am@MacBook-Air-de-Angelica.local>
2021-01-23 22:42:41 -0300

    binomial_distr_I_II

--- 10_days_of_statistics_hacker_rank/day04_binomial_distribution_I.R
+++ 10_days_of_statistics_hacker_rank/day04_binomial_distribution_I.R
@@ -8,12 +8,12 @@ boy <- data[1]
 girl <- data[2]
 p <- boy/(boy+girl)
 q <- girl/(boy+girl)
-n = 6
-x = 3
-ans = 0
+n <- 6
+x <- 3
+ans <- 0
 while (x <= 6){
-    n_x = factorial(n)/(factorial(x)*factorial(n-x))
-    ans = ans + (n_x * (p ** x) * (q ** (n - x)))
-    x = x + 1
+    n_x <- factorial(n)/(factorial(x)*factorial(n-x))
+    ans <- ans + (n_x * (p ** x) * (q ** (n - x)))
+    x <- x + 1
 }
 cat(format(round(ans, 3), nsmall = 1))
---
+++ 10_days_of_statistics_hacker_rank/day04_binomial_distribution_II.R
@@ -0,0 +1,29 @@
+#Hacker Rank
+#10 Days of Statistics
+#Day01
+#Binomial distribution II
+
+data <- scan("/dev/stdin")
+reject <- data[1]
+n <- data[2]
+p <- reject / 100
+q <- 1 - p
+#No more than 2 rejects, x >= 0 and x <= 2
+x <- 0
+ans <- 0
+while (x <= 2){
+    n_x <- factorial(n)/(factorial(x)*factorial(n-x))
+    ans <- ans + (n_x * (p ** x) * (q ** (n - x)))
+    x <- x + 1
+}
+cat(format(round(ans, 3), nsmall = 1))
+cat('\n')
+#At least 2 rejects, x >= 2 and x <= 10
+x <- 2
+ans <- 0
+while (x <= n){
+    n_x <- factorial(n)/(factorial(x)*factorial(n-x))
+    ans <- ans + (n_x * (p ** x) * (q ** (n - x)))
+    x <- x + 1
+}
+cat(format(round(ans, 3), nsmall = 1))
